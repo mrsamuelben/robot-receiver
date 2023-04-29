3e2941146e09ae78546ff32bc5d170400f67dad7
mrsamuelben <101801986+mrsamuelben@users.noreply.github.com>
2023-04-30 00:16:34 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,83 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+        }
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else if (receivedNumber == 2) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+        }
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else if (receivedNumber == 3) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+        }
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else if (receivedNumber == 4) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+        }
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
